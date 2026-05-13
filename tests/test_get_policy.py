5384ee5176df191888e744290fdfbf98087841ab
Colin Meek <colin.meek13@gmail.com>
2026-05-13 08:15:09 -0700

    Remove tests

--- tests/test_get_policy.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from s3_account_search.cli import get_policy
-
-
-def test_list_input():
-    actual = get_policy("1")
-    assert actual.get("Statement")[0].get("Condition").get("StringLike").get("s3:ResourceAccount") == ["1*"]
-
